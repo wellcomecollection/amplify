0dedae7b6547817bf3d6330858e186c810538764
Balazs Feher <balazs.feher@neuralmachines.co.uk>
2019-12-07 17:25:08 +0000

    feat: added config file with PATHs to required files

---
+++ code/config.py
@@ -0,0 +1,9 @@
+GOOGLE_APPLICATION_CREDENTIALS_PATH = "/Users/balazs/Documents/wellcome-amplify-7053884cb4d5.json"
+
+# Sample images in order of increasing complexity
+IMAGE_PATH = ['../data/sample_1.png',
+              '../data/sample_2.jpg',
+              '../data/sample_3.jpg']
+
+WORLDCAT_READ_API_RESPONSE_PATH = ['../data/worldcat_read_api_response_sample_1_1.xml',
+                                   '../data/worldcat_read_api_response_sample_1_2.xml']
