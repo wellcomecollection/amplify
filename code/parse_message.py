2a05406777f94f9fd8fef98b9c58c9482ed7b63c
Balazs Feher <balazs.feher@neuralmachines.co.uk>
2019-12-07 17:22:58 +0000

    feat: wrapped in function

--- code/parse_message.py
+++ code/parse_message.py
@@ -1,29 +1,32 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('../data/response.xml')
-root = tree.getroot()
 
-# print(root.tag)
-# print(root.attrib)
+def parse(path):
 
-for child in root:
-    # print(child.tag, child.attrib)
-    try:
-        tag = child.get('tag')
-        if tag == '245':
-            for subchild in child:
-                code = subchild.get('code')
-                if code == 'a':
-                    print('Title: ' + subchild.text)
-                elif code == 'c':
-                    print('Author: ' + subchild.text)
-        elif tag == '264':
-            for subchild in child:
-                code = subchild.get('code')
-                if code == 'a':
-                    print('Location: ' + subchild.text)
-                elif code == 'b':
-                    print('Publisher: ' + subchild.text)
-                elif code == 'c':
-                    print('Publication Date: ' + subchild.text)
-    except:
-        pass
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    # print(root.tag)
+    # print(root.attrib)
+
+    for child in root:
+        # print(child.tag, child.attrib)
+        try:
+            tag = child.get('tag')
+            if tag == '245':
+                for subchild in child:
+                    code = subchild.get('code')
+                    if code == 'a':
+                        print('Title: ' + subchild.text)
+                    elif code == 'c':
+                        print('Author: ' + subchild.text)
+            elif tag == '264' or tag == '260':
+                for subchild in child:
+                    code = subchild.get('code')
+                    if code == 'a':
+                        print('Location: ' + subchild.text)
+                    elif code == 'b':
+                        print('Publisher: ' + subchild.text)
+                    elif code == 'c':
+                        print('Publication Date: ' + subchild.text)
+        except:
+            pass
