03aad825a9ee78772f8a3ca71d296bc26d1722e9
Balazs Feher <balazs.feher@neuralmachines.co.uk>
2019-12-10 18:33:32 +0000

    docs: added logging

--- code/app.py
+++ code/app.py
@@ -2,17 +2,17 @@ import config
 import parse_message
 import google_vision_api
 
-#Text extraction
+print('Extracting text...')
 response = google_vision_api.detect_text(config.IMAGE_PATH[0])
 print(response)
 
-#Work matching
+print('Searching on WorldCat...')
 #TODO: Search (on WorldCat)
 
-#Retrieve record identifiers (IDs of books on WorldCat)
+print('Retrieving record identifiers (IDs of books) from WorldCat...')
 record_identifier_dict, record_identifier_list = parse_message.get_record_identifiers(config.WORLDCAT_SESRCH_API_RESPONSE_PATH[0])
 print(record_identifier_list, record_identifier_dict)
 
-#Retrieve individual MARC records
+print('Retrieving individual MARC record(s)...')
 path = config.WORLDCAT_READ_API_RESPONSE_PATH[0]
 parse_message.parse(path)
