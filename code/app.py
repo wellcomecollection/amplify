27bf71aadaa8fa3548eaa7220a4ec35716eedc40
Balazs Feher <balazs.feher@neuralmachines.co.uk>
2019-12-12 16:53:13 +0000

    feat: added new method for translating text through google translate API

--- code/app.py
+++ code/app.py
@@ -1,10 +1,15 @@
 import config
 import parse_message
 import google_vision_api
+import google_translate_api
+import library_hub_api
 
 print('Extracting text...')
 response = google_vision_api.detect_text(config.IMAGE_PATH[0])
 print(response)
+
+print('Translating text...')
+result = google_translate_api.translate_text(response)
 
 print('Searching on WorldCat...')
 #TODO: Search (on WorldCat)
@@ -16,3 +21,14 @@ print(record_identifier_list, record_identifier_dict)
 print('Retrieving individual MARC record(s)...')
 path = config.WORLDCAT_READ_API_RESPONSE_PATH[0]
 parse_message.parse(path)
+
+print('Searching for duplicates in Library Hub ...')
+response = library_hub_api.search_record(author='david deutsch', title='the fabric of reality', publisher='penguin books')
+
+print(response.json()['records'][0]['bibliographic_data'])
+print('Author: ' + response.json()['records'][0]['bibliographic_data']['author'][0])
+print('Title: ' + response.json()['records'][0]['bibliographic_data']['title'][0])
+print('Publication details: ' + response.json()['records'][0]['bibliographic_data']['publication_details'][0])
+print('Institution ID: ' + response.json()['records'][0]['holdings'][0]['held_at'][0]['institution']['institution_id'])
+
+print(library_hub_api.checking_for_duplicates_at_wellcome(response, record_index=0))
---
+++ code/google_translate_api.py
@@ -0,0 +1,22 @@
+import config
+import os
+import six
+from google.cloud import translate_v2 as translate
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config.GOOGLE_APPLICATION_CREDENTIALS_PATH
+
+def translate_text(text):
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+
+    # Text can also be a sequence of strings, in which case this method will return a sequence of results for each text.
+    result = translate_client.translate(
+        text, target_language='en')
+
+    print(u'Text: {}'.format(result['input']))
+    print(u'Translation: {}'.format(result['translatedText']))
+    print(u'Detected source language: {}'.format(result['detectedSourceLanguage']))
+
+    return result
