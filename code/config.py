a795286ff4b28289bb34ac69899619bf2033c490
Balazs Feher <balazs.feher@neuralmachines.co.uk>
2019-12-12 16:55:16 +0000

    feat: added new sample to test google translate API

--- code/config.py
+++ code/config.py
@@ -3,7 +3,8 @@ GOOGLE_APPLICATION_CREDENTIALS_PATH = "/Users/balazs/Documents/wellcome-amplify-
 # Sample images in order of increasing complexity
 IMAGE_PATH = ['../data/sample_1.png',
               '../data/sample_2.jpg',
-              '../data/sample_3.jpg']
+              '../data/sample_3.jpg',
+              '../data/sample_4.png']
 
 WORLDCAT_READ_API_RESPONSE_PATH = ['../data/worldcat_read_api_response_sample_1_1.xml',
                                    '../data/worldcat_read_api_response_sample_1_2.xml']
