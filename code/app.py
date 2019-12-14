1cd723ca14ac0098022b755390dc35c8e039d161
Balazs Feher <balazs.feher@neuralmachines.co.uk>
2019-12-14 13:54:13 +0000

    feat: added WorldCat Open Search and Read methods for a full pipeline for simple english titles from image recognition to duplicate checks

--- code/app.py
+++ code/app.py
@@ -3,6 +3,7 @@ import parse_message
 import google_vision_api
 import google_translate_api
 import library_hub_api
+import worldcat_search_api
 
 print('Extracting text...')
 response = google_vision_api.detect_text(config.IMAGE_PATH[0])
@@ -11,19 +12,24 @@ print(response)
 print('Translating text...')
 result = google_translate_api.translate_text(response)
 
-print('Searching on WorldCat...')
-#TODO: Search (on WorldCat)
+print('Running Open Search on WorldCat...')
+response = worldcat_search_api.open_search(response)
 
-print('Retrieving record identifiers (IDs of books) from WorldCat...')
-record_identifier_dict, record_identifier_list = parse_message.get_record_identifiers(config.WORLDCAT_SESRCH_API_RESPONSE_PATH[0])
+print('Retrieving record identifiers from WorldCat Open Search response...')
+# record_identifier_dict, record_identifier_list = parse_message.get_record_identifiers(config.WORLDCAT_SESRCH_API_RESPONSE_PATH[0], from_file=True)
+record_identifier_dict, record_identifier_list = parse_message.get_record_identifiers(response)
 print(record_identifier_list, record_identifier_dict)
 
-print('Retrieving individual MARC record(s)...')
-path = config.WORLDCAT_READ_API_RESPONSE_PATH[0]
-parse_message.parse(path)
+print('Retrieving individual MARC record XML from WorldCat Read endpoint...')
+response = worldcat_search_api.read(record_identifier_list[0])
 
+print('Parse MARC record...')
+# parse_message.parse(config.WORLDCAT_READ_API_RESPONSE_PATH[0], from_file=True)
+author, title, publisher = parse_message.parse(response)
+#
 print('Searching for duplicates in Library Hub ...')
-response = library_hub_api.search_record(author='david deutsch', title='the fabric of reality', publisher='penguin books')
+# response = library_hub_api.search_record(author='david deutsch', title='the fabric of reality', publisher='penguin books')
+response = library_hub_api.search_record(author=author, title=title, publisher=publisher)
 
 print(response.json()['records'][0]['bibliographic_data'])
 print('Author: ' + response.json()['records'][0]['bibliographic_data']['author'][0])
