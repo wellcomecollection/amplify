8d141d1dad62e8d43abd32cfc93cca65931a39d7
Balazs Feher <balazs.feher@neuralmachines.co.uk>
2020-01-16 14:26:33 +0000

    minor: moved images to assets

--- code/config.py
+++ code/config.py
@@ -2,10 +2,11 @@ GOOGLE_APPLICATION_CREDENTIALS_PATH = "/Users/balazs/Documents/wellcome-amplify-
 WORLDCAT_WSKEY = 'cNs4qsGP09f58rs6MxoMwnJVRdZPRX1jc2pn8faRUTzaIATlCRfUdU5XtYYQwqtdDnahUGryoyVG0BpZ'
 
 # Sample images in order of increasing complexity
-IMAGE_PATH = ['../data/sample_1.png',
-              '../data/sample_2.jpg',
-              '../data/sample_3.jpg',
-              '../data/sample_4.png']
+IMAGE_PATH = ['assets/sample_1.png',
+              'assets/sample_2.jpg',
+              'assets/sample_3.jpg',
+              'assets/sample_4.png',
+              'assets/sample_5.1.png']
 
 WORLDCAT_READ_API_RESPONSE_PATH = ['../data/worldcat_read_api_response_sample_1_1.xml',
                                    '../data/worldcat_read_api_response_sample_1_2.xml']
