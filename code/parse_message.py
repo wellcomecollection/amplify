d44a4172318530b8210e714845dde4d5fbdc89c3
Balazs Feher <balazs.feher@neuralmachines.co.uk>
2020-03-08 17:26:25 +0100

    bugfix: fixed get_record_identifiers function as it was double counting records feat: added detailed parsing method to return all details from worldcat responses

--- code/parse_message.py
+++ code/parse_message.py
@@ -12,6 +12,10 @@ def parse(data, from_file=False):
     author = ''
     title = ''
     publisher = ''
+    location = ''
+    publication_date = ''
+
+    # print(data)
 
     if from_file:
         tree = ET.parse(data)
@@ -27,24 +31,56 @@ def parse(data, from_file=False):
                 for subchild in child:
                     code = subchild.get('code')
                     if code == 'a':
-                        print('Title: ' + subchild.text)
+                        # print('Title: ' + subchild.text)
                         title = subchild.text
                     elif code == 'c':
-                        print('Author: ' + subchild.text)
+                        # print('Author: ' + subchild.text)
                         author = subchild.text
             elif tag == '264' or tag == '260':
                 for subchild in child:
                     code = subchild.get('code')
                     if code == 'a':
-                        print('Location: ' + subchild.text)
+                        # print('Location: ' + subchild.text)
+                        location = subchild.text
                     elif code == 'b':
-                        print('Publisher: ' + subchild.text)
+                        # print('Publisher: ' + subchild.text)
                         publisher = subchild.text
                     elif code == 'c':
-                        print('Publication Date: ' + subchild.text)
+                        # print('Publication Date: ' + subchild.text)
+                        publication_date = subchild.text
         except:
             pass
     return author, title, publisher
+
+
+def parse_detailed(data, from_file=False):
+    '''Parse and print individual MARC record retrieved by WorldCat Read API.
+
+    :param data: string, XML or path to XML
+    :param from_file: boolean
+    :return: None
+    '''
+
+    results = []
+
+    # print(data)
+
+    if from_file:
+        tree = ET.parse(data)
+        root = tree.getroot()
+    else:
+        root = ET.fromstring(data)
+
+    for child in root:
+        try:
+            tag = child.get('tag')
+            for subchild in child:
+                code = subchild.get('code')
+                subfield = subchild.text
+                results.append({'tag': tag, 'code': code, 'subfield': subfield})
+        except:
+            pass
+    return results
 
 
 def get_record_identifiers(data, from_file=False):
@@ -72,10 +108,12 @@ def get_record_identifiers(data, from_file=False):
                 if entry.tag == '{http://purl.org/oclc/terms/}recordIdentifier':
                     record_identifier = entry.text
                     record_identifier_list.append(record_identifier)
-            for index in range(len(title_list)):
-                record_identifier_dict_list.append({'record_identifier': record_identifier_list[index], 'title': title_list[index]})
         except:
             pass
+
+    for index in range(len(title_list)):
+        record_identifier_dict_list.append(
+            {'record_identifier': record_identifier_list[index], 'title': title_list[index]})
 
     return record_identifier_dict_list, record_identifier_list
 
