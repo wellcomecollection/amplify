1ed8b094848699e0fba314e00aed19fe1c3599f4
harrisonpim <harrison.stroma@gmail.com>
2020-06-15 12:50:56 +0100

    add basic docker build/orchestration for frontend

--- frontend/src/app/backend-api.service.ts
+++ frontend/src/app/backend-api.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import {timeout, catchError} from 'rxjs/operators';
-import { pythonPost } from './backend.model';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, of, throwError } from "rxjs";
+import { timeout, catchError } from "rxjs/operators";
+import { pythonPost } from "./backend.model";
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  })
+    "Content-Type": "application/json",
+  }),
 };
 
 // const httpOptionsPost = {
@@ -16,44 +16,47 @@ const httpOptions = {
 //   })
 // };
 
-const API_URL = 'http://localhost:4204';
+const API_URL = "http://api:4204";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class BackendApiService {
-
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getVisionOutputStage1(frontPage: any): Observable<any> {
-    return this.http.post<any>(`${API_URL}/visionStage1`, frontPage)
-    .pipe(timeout(86400000));
+    return this.http
+      .post<any>(`${API_URL}/visionStage1`, frontPage)
+      .pipe(timeout(86400000));
   }
 
   searchABIM(ABIMSearchDict: any): Observable<any> {
-    return this.http.post<any>(`${API_URL}/abim_search`, ABIMSearchDict)
-    .pipe(timeout(86400000));
+    return this.http
+      .post<any>(`${API_URL}/abim_search`, ABIMSearchDict)
+      .pipe(timeout(86400000));
   }
 
   libraryHubSearch(data: any): Observable<any> {
-    return this.http.post<any>(`${API_URL}/library_hub_search`, data)
-    .pipe(timeout(86400000));
+    return this.http
+      .post<any>(`${API_URL}/library_hub_search`, data)
+      .pipe(timeout(86400000));
   }
 
   getVisionOutputStage2(backendPost: any): Observable<any> {
-    return this.http.post<any>(`${API_URL}/visionStage2`, backendPost, httpOptions)
-    .pipe(timeout(86400000));
+    return this.http
+      .post<any>(`${API_URL}/visionStage2`, backendPost, httpOptions)
+      .pipe(timeout(86400000));
   }
 
   getVisionOutputStage3(backendPost: any): Observable<any> {
-    return this.http.post<any>(`${API_URL}/visionStage3`, backendPost, httpOptions)
-    .pipe(timeout(86400000));
+    return this.http
+      .post<any>(`${API_URL}/visionStage3`, backendPost, httpOptions)
+      .pipe(timeout(86400000));
   }
 
   postVisionOutput(backendPost: any): Observable<any> {
-    return this.http.post<any>(`${API_URL}/vision_post`, backendPost, httpOptions)
-    .pipe(timeout(86400000));
+    return this.http
+      .post<any>(`${API_URL}/vision_post`, backendPost, httpOptions)
+      .pipe(timeout(86400000));
   }
-
 }
